8d407724c961105a8a379176c2de8900cb2506e6
Tom Georgi <Tom.Georgi@htwg-konstanz.de>
2019-11-28 13:45:12 +0100

    updated drive

--- Laboratory_Two/drive.py
+++ Laboratory_Two/drive.py
@@ -1,12 +1,12 @@
 from time import sleep
+from threading import Thread
 
 import mraa
 import sys
 from evdev import InputDevice, categorize, ecodes
 
-GEAR = 1
-
-#Declare pins
+GEAR_SPEED = [(0.0, 0.20), (0.20, 0.35), (0.35, 0.55), (0.55, 0.80), (0.80, 1.00)]
+# Declare pins
 driveMotorPwm = 0
 driveMotorPin1 = 45
 driveMotorPin2 = 46
@@ -15,7 +15,7 @@ steeringMotorPin1 = 47
 steeringMotorPin2 = 48
 standby = 15
 
-#MRAA Lib pins
+# MRAA Lib pins
 pin_driveMotorPwm = mraa.Pwm(driveMotorPwm)
 pin_steeringMotorPwm = mraa.Pwm(steeringMotorPwm)
 pin_driveMotorPin1 = mraa.Gpio(driveMotorPin1)
@@ -39,6 +39,8 @@ pin_driveMotorPin1.write(0)
 pin_driveMotorPin2.write(0)
 pin_steeringMotorPin1.write(0)
 pin_steeringMotorPin2.write(0)
+
+not_stopped = False
 
 
 def connect_controller():
@@ -86,59 +88,109 @@ def steer(value):
 
 
 def car(con):
-    global GEAR
+    gear = 1
+    # global GEAR_SPEED
+    global not_stopped
 
-    for event in con.read_loop():
-        if event.code == ecodes.BTN_TR:
-            # print("Button TR pressed")
-            if event.value == 1:
-                # print("Drive Value = 1")
-                # pin_standby.write(1)
-                if GEAR == -1:
-                    GEAR = 1
-                elif GEAR < 6:
-                    GEAR += 1
-                # print("PWM Value: ", str(pin_driveMotorPwm.read()))
-            print("Current Gear: ", str(GEAR))
-        if event.code == ecodes.BTN_TL:
-            # print("Button TL pressed")
-            if event.value == 1:
-                if GEAR == 1:
-                    GEAR = -1
-                elif -1 < GEAR <= 6:
-                    GEAR -= 1
+    old_gear = gear
+    drive_value = 0
 
-            print("Current Gear: ", str(GEAR))
-        if event.code == ecodes.ABS_X:
-            if event.value != 0:
-                steer_value = -1 * (event.value / 128.0) + 1.0
-                # print("Linker Trigger  - X Achse " + str(event.value) + " | " + str(steer_value))
-                steer(steer_value)
-        if event.code == ecodes.ABS_RZ:
-            if event.value != 0:
-                drive_value = - (event.value / 128.0) + 1.0
-                # print ("Rechter Trigger - Y Achse " + str(event.value) + " | " + str(drive_value))
-                drive(drive_value)
-        if event.code == ecodes.BTN_B:
-            drive(0)
-            steer(0)
-            sys.exit(0)
-        if event.code == ecodes.BTN_C:
-            # Backwards parking
-            if event.value == 1:
-                steer(-1)
-                drive(-0.3)
-                sleep(2)
-                steer(0)
-                sleep(0.5)
+    try:
+        started = False
+        t_drive = None
+        for event in con.read_loop():
+            if event.code == ecodes.BTN_Z:
+                if event.value == 1:
+                    print("start drift")
+                    if started is False:
+                        t_drive = Thread(target=drift)
+                        t_drive.start()
+                        started = True
+                        not_stopped = True
+                    else:
+                        not_stopped = False
+                        t_drive.join()
+                        del t_drive
+                        started = False
+            if event.code == ecodes.BTN_B:
+                not_stopped = False
                 drive(0)
+                steer(0)
+                sys.exit(0)
 
-        if event.code == ecodes.BTN_A:
-            # Front parking
-            pass
-        if event.code == ecodes.BTN_X:
-            # Side parking
-            pass
+            if not not_stopped:
+                if event.code == ecodes.BTN_TR:
+                    # print("Button TR pressed")
+                    if event.value == 1:
+                        # print("Drive Value = 1")
+                        # pin_standby.write(1)
+                        if gear < 4:
+                            old_gear = gear
+                            gear += 1
+                        # print("PWM Value: ", str(pin_driveMotorPwm.read()))
+                    print("Current Gear: ", str(gear))
+                if event.code == ecodes.BTN_TL:
+                    # print("Button TL pressed")
+                    if event.value == 1:
+                        if gear > 0:
+                            old_gear = gear
+                            gear -= 1
+                    print("Current Gear: ", str(gear))
+                if event.code == ecodes.ABS_X:
+                    if event.value != 0:
+                        steer_value = -1 * (event.value / 128.0) + 1.0
+                        # print("Linker Trigger  - X Achse " + str(event.value) + " | " + str(steer_value))
+                        steer(steer_value)
+                if event.code == ecodes.ABS_RZ:
+                    if event.value != 0:
+                        drive_value = - (event.value / 128.0) + 1.0
+
+                if event.code == ecodes.BTN_C:
+                    # Backwards parking
+                    if event.value == 1:
+                        steer(-1)
+                        drive(-0.3)
+                        sleep(2)
+                        steer(0)
+                        sleep(0.5)
+                        drive(0)
+
+                if event.code == ecodes.BTN_A:
+                    # Front parking
+                    pass
+                if event.code == ecodes.BTN_X:
+                    # Side parking
+                    pass
+
+                values = GEAR_SPEED[gear]
+                if drive_value < 0:
+                    drive(drive_value)
+                else:
+                    if values[0] <= drive_value:
+                        if drive_value >= values[1]:
+                            drive(values[1])
+                        else:
+                            drive(drive_value)
+                    else:
+                        drive(0)
+
+    except KeyboardInterrupt:
+        print("Interrupt by keyboard")
+        exit(0)
+
+
+def drift():
+    global not_stopped
+    steer(1)
+    drive(1)
+    while not_stopped:
+        sleep(2)
+        steer(-1)
+        sleep(2)
+        steer(1)
+    steer(0)
+    drive(0)
+
 
 if __name__ == '__main__':
     controller = connect_controller()
